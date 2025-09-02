1eded100e165c910c1d75c60b5ba59c2b37ec91c
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-02 17:32:57 +0100

    feat(types): add blockrader api models

--- src/types/blockrader_types.py
+++ src/types/blockrader_types.py
@@ -1,11 +1,18 @@
 from datetime import datetime
 from typing import Any, List, Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+
+from src.types.chain_types import Chain
 
 
 class baseBlockRaderType(BaseModel):
     model_config = ConfigDict(from_attributes=True, extra="allow")
+
+
+class baseResponse(baseBlockRaderType):
+    message: str
+    statusCode: int
 
 
 # Common Models
@@ -120,9 +127,71 @@ class Meta(baseBlockRaderType):
     totalPages: int
 
 
-class baseResponse(baseBlockRaderType):
-    message: str
-    statusCode: int
+class TransactionAsset(baseBlockRaderType):
+    address: str
+    createdAt: datetime
+    decimals: int
+    id: str
+    isActive: bool
+    logoUrl: str
+    name: str
+    network: str
+    standard: Optional[str]
+    symbol: str
+    updatedAt: datetime
+
+
+class TransactionAddress(baseBlockRaderType):
+    address: str
+    configurations: Configurations
+    createdAt: datetime
+    derivationPath: Optional[str]
+    id: str
+    isActive: bool
+    metadata: Optional[Any]
+    name: Optional[str]
+    network: str
+    type: str
+    updatedAt: datetime
+
+
+class TransactionData(baseBlockRaderType):
+    address: TransactionAddress
+    amlScreening: AML
+    amount: str
+    amountPaid: str
+    asset: TransactionAsset
+    assetSwept: bool
+    assetSweptAmount: Optional[str] = None
+    assetSweptAt: Optional[datetime] = None
+    assetSweptGasFee: Optional[str] = None
+    assetSweptHash: Optional[str] = None
+    assetSweptRecipientAddress: Optional[str] = None
+    assetSweptSenderAddress: Optional[str] = None
+    blockHash: Optional[str] = None
+    blockNumber: Optional[int] = None
+    blockchain: Blockchain
+    chainId: int
+    confirmations: int
+    confirmed: bool
+    createdAt: datetime
+    currency: str
+    fee: Optional[str] = None
+    gasFee: str
+    gasPrice: str
+    gasUsed: str
+    hash: str
+    id: str
+    metadata: Optional[Any] = None
+    network: str
+    note: Optional[Any] = None
+    reason: Optional[str] = None
+    recipientAddress: str
+    reference: str
+    senderAddress: str
+    status: str
+    type: str
+    updatedAt: datetime
 
 
 class WalletAddressResponse(baseResponse):
@@ -141,3 +210,101 @@ class WalletBalanceResponse(baseResponse):
 
 class WalletAddressDetailResponse(baseResponse):
     data: Data | None = None
+
+
+class TransactionResponse(baseResponse):
+    data: List[TransactionData] | TransactionData | None = None
+    meta: Meta | None = None
+
+
+class NetworkFeeData(baseBlockRaderType):
+    nativeBalance: str
+    networkFee: str
+
+
+class NetworkFeeResponse(baseResponse):
+    data: NetworkFeeData | None = None
+
+
+class WalletInfo(baseBlockRaderType):
+    id: str
+
+
+class WithdrawalData(baseBlockRaderType):
+    amlScreening: Optional[AML] = None
+    amount: str
+    amountPaid: str
+    asset: AssetInner
+    assetSwept: Optional[bool] = None
+    assetSweptAmount: Optional[str] = None
+    assetSweptAt: Optional[datetime] = None
+    assetSweptGasFee: Optional[str] = None
+    assetSweptHash: Optional[str] = None
+    assetSweptRecipientAddress: Optional[str] = None
+    assetSweptResponse: Optional[Any] = None
+    assetSweptSenderAddress: Optional[str] = None
+    blockHash: Optional[str] = None
+    blockNumber: Optional[int] = None
+    blockchain: Blockchain
+    chainId: Optional[int] = None
+    confirmations: Optional[int] = None
+    confirmed: bool
+    createdAt: datetime
+    currency: str
+    fee: Optional[str] = None
+    feeMetadata: Optional[Any] = None
+    gasFee: Optional[str] = None
+    gasPrice: Optional[str] = None
+    gasUsed: Optional[str] = None
+    hash: str
+    id: str
+    metadata: Optional[Any] = None
+    network: str
+    note: Optional[Any] = None
+    reason: Optional[str] = None
+    recipientAddress: str
+    senderAddress: str
+    status: str
+    tokenAddress: Optional[str] = None
+    type: str
+    updatedAt: datetime
+    wallet: WalletInfo
+
+
+class WithdrawalResponse(baseResponse):
+    data: WithdrawalData | None = None
+
+
+class CreateAddressRequest(baseBlockRaderType):
+    metadata: dict[str, any]
+    name: str
+    disableAutoSweep: bool = Field(default=False)
+    enableGaslessWithdraw: bool = Field(default=True)
+    showPrivateKey: bool = Field(default=False)
+
+
+class NetworkFeeRequest(baseBlockRaderType):
+    address: str
+    amount: str
+    assetId: str
+
+
+class WithdrawalRequest(baseBlockRaderType):
+    assetId: str
+    address: str
+    amount: str
+    reference: Optional[str] = None
+    metadata: Optional[dict[str, Any]] = None
+
+
+class AMLCheckData(baseBlockRaderType):
+    isBlacklisted: bool
+
+
+class AMLCheckResponse(baseResponse):
+    data: AMLCheckData | None = None
+
+
+class AMLCheckRequest(baseBlockRaderType):
+    address: str
+    blockchain: Chain
