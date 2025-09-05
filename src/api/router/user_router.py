b939b891547e5f447dff548b5f7082f77273e2bf
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-05 14:17:17 +0100

    ref: update API layer for new models and repositories

--- src/api/router/user_router.py
+++ src/api/router/user_router.py
@@ -1,13 +1,12 @@
-# src/looprail_backend/router/user_router.py
 from fastapi import APIRouter
 
-from ..handlers import user_handlers
-from ...dtos import user_dtos
+from src.api.handlers import user_handlers
+from src.dtos import user_dtos
 
 # Create a new router for user-related endpoints
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["Users"])
 
 # --- API Endpoints ---
 
-router.post("/users/", response_model=user_dtos.UserPublic, status_code=201)(user_handlers.create_user)
-router.get("/users/{user_id}", response_model=user_dtos.UserPublic)(user_handlers.get_user)
+router.post("/", response_model=user_dtos.UserPublic, status_code=201)(user_handlers.create_user)
+router.get("/{user_id}", response_model=user_dtos.UserPublic)(user_handlers.get_user)
