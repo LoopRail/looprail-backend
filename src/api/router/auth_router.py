547cf4bac0578daddb175ca590c9c98a7b762726
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-05 14:43:23 +0100

    ref: move create_user endpoint to auth router

--- src/api/router/auth_router.py
+++ src/api/router/auth_router.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
+from uuid import UUID
 
-from src.api.router.user_router import create_user
-from src.dtos import user_dtos
+from fastapi import APIRouter, Depends, HTTPException
+
+from src.usecases.user_usecases import UserUseCases
+from src.api.dependencies import get_user_usecases
+from src.dtos.user_dtos import UserCreate, UserPublic
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
+
+async def create_user(
+    user_data: UserCreate, user_usecases: UserUseCases = Depends(get_user_usecases)
+) -> UserPublic:
+    """API endpoint to create a new user."""
+    created_user, err = await user_usecases.create_user(user_create=user_data)
+    if err:
+        raise HTTPException(status_code=400, detail=err.message)
+    return created_user
 
 router.post("/register", response_model=user_dtos.UserPublic, status_code=201)(create_user)
--- src/api/router/user_router.py
+++ src/api/router/user_router.py
@@ -4,22 +4,12 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from src.usecases.user_usecases import UserUseCases
 from src.api.dependencies import get_user_usecases
-from src.dtos.user_dtos import UserCreate, UserPublic
+from src.dtos.user_dtos import UserPublic
 
 # Create a new router for user-related endpoints
 router = APIRouter(prefix="/users", tags=["Users"])
 
 # --- API Endpoints ---
-
-async def create_user(
-    user_data: UserCreate, user_usecases: UserUseCases = Depends(get_user_usecases)
-) -> UserPublic:
-    """API endpoint to create a new user."""
-    created_user, err = await user_usecases.create_user(user_create=user_data)
-    if err:
-        raise HTTPException(status_code=400, detail=err.message)
-    return created_user
-
 
 async def get_user(
     user_id: UUID, user_usecases: UserUseCases = Depends(get_user_usecases)
@@ -30,5 +20,4 @@ async def get_user(
         raise HTTPException(status_code=404, detail=err.message)
     return user
 
-router.post("/", response_model=user_dtos.UserPublic, status_code=201)(create_user)
 router.get("/{user_id}", response_model=user_dtos.UserPublic)(get_user)
