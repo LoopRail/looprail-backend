9b0812fd4982e56754eddbfc8d14518cd7e9c34f
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-05 14:53:57 +0100

    chore: add logging for user creation process

--- src/api/router/auth_router.py
+++ src/api/router/auth_router.py
@@ -1,11 +1,9 @@
-from uuid import UUID
-
 from fastapi import APIRouter, Depends, HTTPException
 
 from src.api.dependencies import get_user_usecases
 from src.dtos.user_dtos import UserCreate, UserPublic
-from src.usecases.user_usecases import UserUseCase
 from src.infrastructure.logger import get_logger
+from src.usecases.user_usecases import UserUseCase
 
 logger = get_logger(__name__)
 
@@ -18,7 +16,9 @@ async def create_user(
     """API endpoint to create a new user."""
     created_user, err = await user_usecases.create_user(user_create=user_data)
     if err:
+        logger.error(f"Failed to create user: {err.message}")
         raise HTTPException(status_code=400, detail=err.message)
+    logger.info(f"User {created_user.username} registered successfully.")
     return created_user
 
 
