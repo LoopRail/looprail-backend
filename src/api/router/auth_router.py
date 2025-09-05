dbe1f56817a46626a19c92968683a53da8fab61a
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-05 14:40:45 +0100

    ref: merge handlers into routers and remove handler directory

--- src/api/router/auth_router.py
+++ src/api/router/auth_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from src.api.handlers import user_handlers
+
+from src.api.router.user_router import create_user
 from src.dtos import user_dtos
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-router.post("/register", response_model=user_dtos.UserPublic, status_code=201)(user_handlers.create_user)
+router.post("/register", response_model=user_dtos.UserPublic, status_code=201)(create_user)
--- src/api/router/user_router.py
+++ src/api/router/user_router.py
@@ -1,12 +1,34 @@
-from fastapi import APIRouter
+from uuid import UUID
 
-from src.api.handlers import user_handlers
-from src.dtos import user_dtos
+from fastapi import APIRouter, Depends, HTTPException
+
+from src.usecases.user_usecases import UserUseCases
+from src.api.dependencies import get_user_usecases
+from src.dtos.user_dtos import UserCreate, UserPublic
 
 # Create a new router for user-related endpoints
 router = APIRouter(prefix="/users", tags=["Users"])
 
 # --- API Endpoints ---
 
-router.post("/", response_model=user_dtos.UserPublic, status_code=201)(user_handlers.create_user)
-router.get("/{user_id}", response_model=user_dtos.UserPublic)(user_handlers.get_user)
+async def create_user(
+    user_data: UserCreate, user_usecases: UserUseCases = Depends(get_user_usecases)
+) -> UserPublic:
+    """API endpoint to create a new user."""
+    created_user, err = await user_usecases.create_user(user_create=user_data)
+    if err:
+        raise HTTPException(status_code=400, detail=err.message)
+    return created_user
+
+
+async def get_user(
+    user_id: UUID, user_usecases: UserUseCases = Depends(get_user_usecases)
+) -> UserPublic:
+    """API endpoint to retrieve a user by their ID."""
+    user, err = await user_usecases.get_user_by_id(user_id)
+    if err:
+        raise HTTPException(status_code=404, detail=err.message)
+    return user
+
+router.post("/", response_model=user_dtos.UserPublic, status_code=201)(create_user)
+router.get("/{user_id}", response_model=user_dtos.UserPublic)(get_user)
