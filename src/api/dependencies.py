48871b30293f67d4911a29f5359774c2fa09ec6d
emperorsixpacks <andrewdvd10@gmail.com>
2025-10-24 10:27:27 +0100

    feat: refactor API clients and services

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,11 +1,14 @@
-
 from fastapi import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.infrastructure.db import get_session
 from src.infrastructure.repositories import (SQLUserRepository,
                                              SQLWalletRepository)
-from src.infrastructure.settings import BlockRaderConfig, block_rader_config
+from src.infrastructure.services.paycrest.paycrest_service import \
+    PaycrestService
+from src.infrastructure.services.paystack_client import PaystackService
+from src.infrastructure.settings import (BlockRaderConfig, block_rader_config,
+                                         paycrest_config, paystack_config)
 from src.models.user_model import UserRepository
 from src.models.wallet_model import WalletRepository
 from src.usecases.user_usecases import UserUseCase
@@ -25,6 +28,14 @@ async def get_wallet_repository(
 
 def get_blockrader_config() -> BlockRaderConfig:
     return block_rader_config
+
+
+async def get_paycrest_service() -> PaycrestService:
+    yield PaycrestService(paycrest_config)
+
+
+async def get_paystack_service() -> PaystackService:
+    yield PaystackService(paystack_config)
 
 
 async def get_user_usecases(
--- src/infrastructure/services/paycrest/paycrest_service.py
+++ src/infrastructure/services/paycrest/paycrest_service.py
@@ -1,40 +1,42 @@
 from decimal import Decimal
-from typing import Optional, Tuple
+from typing import Any, Optional, Tuple
 
-import requests
+from pydantic import BaseModel
 
 from src.dtos import OrderResponse
+from src.infrastructure.services.base_client import BaseClient
 from src.infrastructure.settings import PayCrestConfig
 from src.types import Error, error
+from src.types.paycrest_types import VerifyAccountResponse
 
 PAYCREST_API_VERSION = "v1"
-
 BASE_URL = f"https://api.paycrest.io/{PAYCREST_API_VERSION}"
 
 
-class PaycrestService:
-    def __init__(self, config: PayCrestConfig):
-        self.api_key = config.paycrest_api_key
-        self.headers = {
-            "API-Key": self.api_key,
+class PaycrestClient(BaseClient):
+    """A base client for interacting with the Paycrest API."""
+
+    def __init__(self, config: PayCrestConfig) -> None:
+        """Initializes the Paycrest client.
+
+        Args:
+            config: The Paycrest configuration.
+            path: The base path for the API endpoints.
+        """
+        self.config = config
+        super().__init__("")
+
+    def _get_base_url(self) -> str:
+        return BASE_URL
+
+    def _get_headers(self) -> dict[str, str]:
+        return {
+            "API-Key": self.config.paycrest_api_key,
             "Content-Type": "application/json",
         }
 
-    def fetch_rate(
-        self, token: str, amount: Decimal, currency: str, network: str
-    ) -> Tuple[Optional[str], Error]:  # We do not want to break execution
-        """Fetch conversion rate for token -> fiat"""
-        url = f"{BASE_URL}/rates/{token}/{float(amount)}/{currency}"
-        params = {"network": network}
 
-        resp = requests.get(
-            url, headers={"Content-Type": "application/json"}, params=params, timeout=30
-        )
-        if resp.status_code != 200:
-            return None, error(f"Rate fetch failed {resp.status_code}: {resp.text}")
-
-        return resp.json()["data"], None
-
+class PaycrestService(PaycrestClient):
     async def create_payment_order(
         self,
         user_id: int,
@@ -49,12 +51,12 @@ class PaycrestService:
 
         currency = recipient.get("currency")
         if not currency:
-            raise ValueError("Recipient dict must contain a 'currency' key")
+            return None, error("Recipient dict must contain a 'currency' key")
 
-        # Fetch latest rate
-        rate = self.fetch_rate(token, amount, currency, network)
+        rate, err = await self.fetch_rate(token, amount, currency, network)
+        if err:
+            return None, err
 
-        # Build payload
         order_data = {
             "amount": float(amount),
             "token": token,
@@ -65,31 +67,47 @@ class PaycrestService:
             "returnAddress": return_address,
         }
 
-        # Call Paycrest API
-        resp = requests.post(
-            f"{BASE_URL}/sender/orders",
-            headers=self.headers,
-            json=order_data,
-            timeout=30,
+        response, err = await self._post(
+            OrderResponse, path_suffix="/sender/orders", data=order_data
         )
+        if err:
+            return None, err
 
-        if resp.status_code != 200:
-            return None, error(f"Order creation failed {resp.status_code}: {resp.text}")
+        if response:
+            response.user_id = user_id
 
-        data = resp.json()
-        order_id = data.get("id")
-        receive_address = data.get("receiveAddress")
-        sender_fee = data.get("senderFee")
-        transaction_fee = data.get("transactionFee")
-        valid_until = data.get("validUntil")
+        return response, None
 
-        # Return response object
-        return OrderResponse(
-            user_id=user_id,
-            order_id=order_id,
-            amount=amount,
-            receive_address=receive_address,
-            sender_fee=sender_fee,
-            transaction_fee=transaction_fee,
-            valid_until=valid_until,
-        ), None
+    async def verify_account(
+        self, account_number: str, institution: str
+    ) -> Tuple[Optional[Any], Error]:
+        """Verify bank account details"""
+
+        data = {"institution": institution, "accountIdentifier": account_number}
+        response, err = await self._post(
+            VerifyAccountResponse, path_suffix="/verify-account", data=data
+        )
+        if err:
+            return None, err
+        return response, None
+
+    async def fetch_letest_usdc_rate(
+        self, amount: float, currency: str
+    ) -> Tuple[Optional[VerifyAccountResponse], Error]:
+        """Verify bank account details"""
+
+        data = {
+            "token": "USDC",
+            "amount": amount,
+            "currency": currency,
+        }
+        response, err = await self._get(
+            VerifyAccountResponse,
+            path_suffix="/rates/{token}/{amount}/{currency}".format(**data),
+            req_params={
+                "network": "base",
+            },
+        )
+        if err:
+            return None, err
+        return response, None
