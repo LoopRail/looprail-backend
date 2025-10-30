f8dd224030d7085c4fbfaf88af72c12b79e8c7bd
emperorsixpacks <andrewdvd10@gmail.com>
2025-10-30 15:10:18 +0100

    refactor: add __all__ to api dependencies __init__.py

--- src/api/dependencies/__init__.py
+++ src/api/dependencies/__init__.py
@@ -4,8 +4,21 @@ from src.api.dependencies.services import (
     get_paycrest_service,
     get_paystack_service,
     get_redis_service,
+    get_resend_service,
 )
 from src.api.dependencies.usecases import (
     get_otp_usecase,
     get_user_usecases,
 )
+
+__all__ = [
+    "get_user_repository",
+    "get_wallet_repository",
+    "get_blockrader_config",
+    "get_paycrest_service",
+    "get_paystack_service",
+    "get_redis_service",
+    "get_resend_service",
+    "get_otp_usecase",
+    "get_user_usecases",
+]
