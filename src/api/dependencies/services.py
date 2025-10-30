9733534298319dd07e5223ebaad995a6557a5250
emperorsixpacks <andrewdvd10@gmail.com>
2025-10-30 10:15:58 +0100

    fix: ensure services.py is correctly updated

--- src/api/dependencies/services.py
+++ src/api/dependencies/services.py
@@ -1,14 +1,12 @@
 from src.infrastructure import RedisClient, redis_config
-from src.infrastructure.services.paycrest.paycrest_service import PaycrestService
+from src.infrastructure.services import ResendService
+from src.infrastructure.services.paycrest.paycrest_service import \
+    PaycrestService
 from src.infrastructure.services.paystack_client import PaystackService
-from src.infrastructure.settings import (
-    BlockRaderConfig,
-    block_rader_config,
-    paycrest_config,
-    paystack_config,
-    ResendConfig,
-    resend_config,
-)
+from src.infrastructure.settings import (BlockRaderConfig, block_rader_config,
+                                         paycrest_config, paystack_config,
+                                         resend_config)
+
 
 def get_blockrader_config() -> BlockRaderConfig:
     return block_rader_config
