6f1fb8a44078c6df181ca50c496eb65933c3bc0e
emperorsixpacks <andrewdvd10@gmail.com>
2025-10-30 15:08:42 +0100

    feat: add OtpCreate DTO to __init__.py

--- src/dtos/__init__.py
+++ src/dtos/__init__.py
@@ -1,3 +1,13 @@
 from src.dtos.offramp_dto import OrderResponse, OrderRequest
 from src.dtos.account_dtos import VerifyAccountRequest 
 from src.dtos.payment_dtos import PaymentStatusResponse, paymentDetails
+from src.dtos.otp_dtos import OtpCreate
+
+__all__ = [
+    "OrderResponse",
+    "OrderRequest",
+    "VerifyAccountRequest",
+    "PaymentStatusResponse",
+    "paymentDetails",
+    "OtpCreate",
+]
