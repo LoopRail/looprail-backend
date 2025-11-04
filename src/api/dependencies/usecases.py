a5617de3c91440453c0e00b87685e71444c24467
emperorsixpacks <andrewdvd10@gmail.com>
2025-11-04 08:53:26 +0100

    feat: add otp token dependency and update email service

--- src/api/dependencies/usecases.py
+++ src/api/dependencies/usecases.py
@@ -1,11 +1,14 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException, Request
 
-from src.api.dependencies.repositories import get_user_repository, get_wallet_repository
-from src.api.dependencies.services import get_blockrader_config, get_redis_service
+from src.api.dependencies.repositories import (get_user_repository,
+                                               get_wallet_repository)
+from src.api.dependencies.services import (get_blockrader_config,
+                                           get_redis_service)
 from src.infrastructure import RedisClient, otp_config
 from src.infrastructure.settings import BlockRaderConfig
 from src.models.user_model import UserRepository
 from src.models.wallet_model import WalletRepository
+from src.types import OtpType
 from src.usecases import OtpUseCase, UserUseCase
 
 
@@ -21,3 +24,13 @@ async def get_otp_usecase(
     redis_client: RedisClient = Depends(get_redis_service),
 ) -> OtpUseCase:
     yield OtpUseCase(redis_client, otp_config)
+
+
+async def get_otp_token(
+    request: Request = Depends(), otp_usecase: OtpUseCase = Depends(get_otp_usecase)
+):
+    token = request.headers.get("X-OTP-Token")
+    if token is None:
+        raise HTTPException(status_code=400, detail="No OTP found or expired")
+    token = await otp_usecase.get_otp(token, token_type=OtpType.EMAIL_VERIFICATION)
+    return token
