e3b2cbd0871e7ede3efd903f0ac577f303e4864d
emperorsixpacks <andrewdvd10@gmail.com>
2025-11-13 17:11:49 +0100

    feat: Add SQLProviderRepository to __init__.py

--- src/infrastructure/repositories/__init__.py
+++ src/infrastructure/repositories/__init__.py
@@ -1,4 +1,5 @@
 from .user_repository import SQLUserRepository
 from .wallet_repository import SQLWalletRepository
+from .provider_repository import SQLProviderRepository
 
-__all__ = ["SQLUserRepository", "SQLWalletRepository"]
+__all__ = ["SQLUserRepository", "SQLWalletRepository", "SQLProviderRepository"]
