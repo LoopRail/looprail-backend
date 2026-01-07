5fe77f227dccad2a1d592c5836786957c654aa22
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-07 01:57:59 +0100

    feat: add get_all_country_codes function

--- src/utils/country_utils.py
+++ src/utils/country_utils.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.types.country_types import CountriesData, CountryInfo
 
 
@@ -10,3 +12,12 @@ def get_country_info(
     countries: CountriesData, /, country_code: str
 ) -> CountryInfo | None:
     return countries.countries.get(country_code.upper())
+
+
+def get_all_country_codes(countries: CountriesData) -> List[str]:
+    """
+    Returns a list of all enabled country codes.
+    """
+    return [
+        code for code, info in countries.countries.items() if info.enabled
+    ]
