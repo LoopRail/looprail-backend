d987bbaa4edf7a2a62c16c500f62e871b6bdb677
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-07 02:16:05 +0100

    feat: use regex for EVM address validation in Address type

--- src/types/common_types.py
+++ src/types/common_types.py
@@ -1,18 +1,22 @@
+import re
 from typing import Annotated, Literal, Union
 
 import phonenumbers
 from pydantic import BeforeValidator
 from pydantic_extra_types.phone_numbers import PhoneNumberValidator
 from solana.publickey import PublicKey
-from web3 import Web3
 
 from src.infrastructure import config
 from src.utils.country_utils import get_all_country_codes
 
+_HEX_ADDRESS_REGEXP = re.compile("(0x)?[0-9a-f]{40}", re.IGNORECASE | re.ASCII)
+
 
 def validate_address(v: str) -> str:
-    if Web3.is_address(v):
+    # Validate as EVM address using regex
+    if _HEX_ADDRESS_REGEXP.fullmatch(v):
         return v
+    # Validate as Solana address
     try:
         PublicKey(v)
         return v
