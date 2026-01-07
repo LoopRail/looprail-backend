6f03551c0a3ff2702e7005483eef42a97ef23c13
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-07 02:00:23 +0100

    feat: dynamically load supported regions for PhoneNumberValidator

--- src/types/common_types.py
+++ src/types/common_types.py
@@ -3,9 +3,14 @@ from typing import Annotated, Literal, Union
 import phonenumbers
 from pydantic_extra_types.phone_numbers import PhoneNumberValidator
 
+from src.infrastructure import config
+from src.utils.country_utils import get_all_country_codes
+
 DeletionFilter = Literal["all", "deleted", "active"]
+
+enabled_country_codes = get_all_country_codes(config.countries)
 
 CommonPhoneNumber = Annotated[
     Union[str, phonenumbers.PhoneNumber],
-    PhoneNumberValidator(supported_regions=["US"], default_region="US"),
+    PhoneNumberValidator(supported_regions=enabled_country_codes, default_region="US"),
 ]
