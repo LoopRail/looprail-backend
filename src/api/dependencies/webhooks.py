499a5d7d6d36c4230fdd1d9c5bc5d89e4966ff30
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-09 02:50:10 +0100

    revert: webhook event dependency refactor

--- src/api/dependencies/webhooks.py
+++ src/api/dependencies/webhooks.py
@@ -1,23 +1,14 @@
-from typing import Tuple
+from fastapi import Request, status
 
-from fastapi import Depends, status
-
-from src.api.dependencies.extra_deps import VerifyWebhookRequest
-from src.api.dependencies.usecases import get_secrets_usecase
 from src.infrastructure.logger import get_logger
 from src.types import httpError
 from src.types.blockrader import GenericWebhookEvent, WebhookEvent
-from src.usecases.secrets_usecases import WebhookProvider
 
 logger = get_logger(__name__)
 
 
-async def get_blockrader_webhook_event(
-    verified_webhook: Tuple[WebhookProvider, bytes] = Depends(
-        VerifyWebhookRequest(secrets_usecase=Depends(get_secrets_usecase))
-    ),
-) -> WebhookEvent:
-    provider, body = verified_webhook
+async def get_blockrader_webhook_event(request: Request) -> WebhookEvent:
+    body = request.json()
 
     try:
         generic_event = GenericWebhookEvent.model_validate_json(body)
