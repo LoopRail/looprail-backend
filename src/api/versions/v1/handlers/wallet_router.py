03580d5d3075654957c9a561fc0d4d4a53f12f6d
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-11 22:28:51 +0100

    feat: add WithdrawalRequest DTO and get_current_user dependency to wallet_router

--- src/api/versions/v1/handlers/wallet_router.py
+++ src/api/versions/v1/handlers/wallet_router.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
+from src.api.dependencies import get_current_user, get_user_usecases
+from src.dtos import WithdrawalRequest
 from src.infrastructure.logger import get_logger
+from src.models import User
+from src.usecases import WalletManagerUsecase
 
 logger = get_logger(__name__)
 
@@ -8,5 +12,8 @@ router = APIRouter(prefix="/wallets", tags=["Wallets"])
 
 
 @router.post("/withdraw", status_code=status.HTTP_200_OK)
-async def withdraw():
+async def withdraw(
+    req: WithdrawalRequest,
+    user: User = Depends(get_current_user),
+):
     pass
