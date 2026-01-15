e62874014b3736a00feb81ad7b5ed5ebf386b513
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-15 03:27:31 +0100

    ref: update auth_deps.py to use new exception structure

--- src/api/dependencies/auth_deps.py
+++ src/api/dependencies/auth_deps.py
@@ -9,11 +9,19 @@ from src.api.dependencies.usecases import (
     get_otp_usecase,
     get_user_usecases,
 )
-from src.api.exceptions import AuthError, OTPError
 from src.dtos import VerifyOtpRequest
 from src.infrastructure import get_logger
 from src.models import Otp, User
-from src.types import AccessToken, Error, OtpStatus, TokenType, httpError
+from src.types import (
+    AccessToken,
+    AuthError,
+    Error,
+    InternaleServerError,
+    OTPError,
+    OtpStatus,
+    TokenType,
+    httpError,
+)
 from src.usecases import JWTUsecase, OtpUseCase, UserUseCase
 from src.usecases.secrets_usecases import SecretsUsecase, WebhookProvider
 from src.utils import verify_signature
@@ -35,26 +43,26 @@ class BearerToken[T]:
         """
         if credentials is None:
             raise AuthError(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={"error": "Authorization header missing"},
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Authorization header missing",
             )
 
         if credentials.scheme.lower() != "bearer":
             raise AuthError(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={"error": "Invalid or expired onboarding token"},
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Invalid or expired onboarding token",
             )
 
         token = credentials.credentials.strip()
         if not token:
             raise AuthError(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={"error": "Token missing"},
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Token missing",
             )
 
         response_token, err = jwt_usecase.verify_token(token, T)
         if err:
-            raise AuthError(status_code=401, detail={"error": "Invalid token"})
+            raise AuthError(code=401, message="Invalid token")
 
         return response_token
 
@@ -63,7 +71,7 @@ async def get_current_user_token(
     token: AccessToken = Depends(BearerToken[AccessToken]()),
 ) -> AccessToken:
     if token.token_type != TokenType.ACCESS_TOKEN:
-        raise AuthError(status_code=401, detail={"error": "User not found"})
+        raise AuthError(code=401, message="User not found")
     return token
 
 
@@ -73,7 +81,7 @@ async def get_current_user(
 ) -> User:
     user, err = await user_usecase.get_user_by_id(access_token.sub)
     if err:
-        raise AuthError(status_code=401, detail={"error": "User not found"})
+        raise AuthError(code=401, message="User not found")
     return user
 
 
@@ -87,34 +95,38 @@ async def verify_otp_dep(
     otp, err = await otp_usecases.get_otp(otp_token, req.otp_type)
     if err:
         logger.error("Error getting otp Error: %s", err)
-        raise OTPError(status_code=400, detail={"error": "Invalid OTP token"})
+        raise OTPError("Invalid OTP token")
 
     if otp.is_expired():
         otp.status = OtpStatus.EXPIRED
-        await otp_usecases.delete_otp(otp_token, req.otp_type.value)
-        raise OTPError(status_code=400, detail={"error": "OTP expired"})
+        err = await otp_usecases.delete_otp(otp.user_email)
+        if err:
+            logger.error("Could not delete OTP %s Error %s", otp_token, err)
+        raise OTPError("OTP expired")
 
     otp.attempts += 1
     if otp.attempts > 30:
         otp.status = OtpStatus.ATTEMPT_EXCEEDED
-        await otp_usecases.delete_otp(otp.user_email)
+        err = await otp_usecases.delete_otp(otp.user_email)
+        if err:
+            logger.error("Could not delete OTP %s Error %s", otp_token, err)
         logger.error("%s exceeded max attempts", otp_token)
-        raise OTPError(status_code=400, detail={"error": "Invalid OTP"})
+        raise OTPError("Invalid OTP")
 
     err = await otp_usecases.update_otp(otp_token, otp)
     if err:
         logger.error("Error updating otp Error: %s", err)
-        raise OTPError(status_code=500, detail={"error": "Internal Server Error"})
+        raise InternaleServerError
 
     is_valid = await otp_usecases.verify_code(req.code, otp.code_hash)
     if not is_valid:
-        raise OTPError(status_code=400, detail={"error": "Invalid OTP"})
+        raise OTPError("Invalid OTP")
 
     otp.status = OtpStatus.USED
     err = await otp_usecases.delete_otp(otp.user_email)
     if err:
         logger.error("Error deleting otp Error: %s", err)
-        raise OTPError(status_code=500, detail={"error": "Internal Server Error"})
+        raise InternaleServerError
     return otp
 
 
