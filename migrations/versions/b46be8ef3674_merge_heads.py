632e69953dc9b190c3b91bb8cebf56c6eef6c9bd
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-17 01:12:13 +0100

    chore: migration

---
+++ migrations/versions/b46be8ef3674_merge_heads.py
@@ -0,0 +1,27 @@
+"""merge heads
+
+Revision ID: b46be8ef3674
+Revises: 7b288f82d3ac, a04f0f42db98
+Create Date: 2026-01-17 01:08:16.431037
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b46be8ef3674'
+down_revision: Union[str, None] = ('7b288f82d3ac', 'a04f0f42db98')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
