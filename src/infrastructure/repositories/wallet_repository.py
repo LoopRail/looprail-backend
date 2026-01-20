68c9c6c378122aef05d1a9aa2835cf976b216ba3
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-20 05:06:27 +0100

    ref: apply base repository refactor to wallet repository

--- src/infrastructure/repositories/wallet_repository.py
+++ src/infrastructure/repositories/wallet_repository.py
@@ -11,28 +11,30 @@ class WalletRepository(Base):
     Concrete implementation of the wallet repository using SQLModel.
     """
 
+    _model = Wallet
+
     async def create_wallet(self, *, wallet: Wallet) -> Tuple[Optional[Wallet], Error]:
         return await self.create(wallet)
 
     async def get_wallet_by_id(
         self, *, wallet_id: WalletId
     ) -> Tuple[Optional[Wallet], Error]:
-        return await self.get(Wallet, wallet_id)
+        return await self.get(wallet_id)
 
     async def get_wallet_by_address(
         self, *, address: str
     ) -> Tuple[Optional[Wallet], Error]:
-        return await self.find_one(Wallet, address=address)
+        return await self.find_one(address=address)
 
     async def get_wallet_by_provider_id(
         self, *, provider_id: str
     ) -> Tuple[Optional[Wallet], Error]:
-        return await self.find_one(Wallet, provider_id=provider_id)
+        return await self.find_one(provider_id=provider_id)
 
     async def get_wallet_by_user_id(
         self, *, user_id: UserId
     ) -> Tuple[Optional[Wallet], Error]:
-        return await self.find_one(Wallet, user_id=user_id)
+        return await self.find_one(user_id=user_id)
 
     async def update_wallet(self, *, wallet: Wallet) -> Tuple[Optional[Wallet], Error]:
         return await self.update(wallet)
