be72e088000a6596f05a32b5483b07c28832f6f8
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-30 00:29:42 +0100

    feat: Add __init__.py for withdrawal handlers

--- src/usecases/withdrawal_handlers/__init__.py
+++ src/usecases/withdrawal_handlers/__init__.py
@@ -1,1 +1,7 @@
+from src.usecases.withdrawal_handlers.bank_transfer_handler import \
+    handle_bank_transfer
+from src.usecases.withdrawal_handlers.external_wallet_handler import \
+    handle_external_wallet_transfer
+from src.usecases.withdrawal_handlers.registry import WithdrawalHandlerRegistry
 
+__alL__ = ["WithdrawalHandlerRegistry"]
