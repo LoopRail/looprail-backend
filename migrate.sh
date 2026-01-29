21d718a95a536675eeddbd6fdd239db542a336d0
emperorsixpacks <andrewdvd10@gmail.com>
2026-01-29 02:56:55 +0100

    feat: add environment flags to migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-uv run alembic upgrade head
+
+ENVIRONMENT="dev" 
+
+for i in "$@"; do
+  case $i in
+    --prod)
+      ENVIRONMENT="prod"
+      shift 
+      ;;
+    --staging)
+      ENVIRONMENT="staging"
+      shift 
+      ;;
+    --dev)
+      ENVIRONMENT="dev"
+      shift 
+      ;;
+    *)
+      ;;
+  esac
+done
+
+echo "Running migrations for environment: $ENVIRONMENT"
+ENVIRONMENT=$ENVIRONMENT uv run alembic upgrade head
+
